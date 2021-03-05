5e6c0cf1df797d4673be41341ead2b7b403949f6
Raghad <raghadmustafa96@yahoo.com>
2021-03-05 19:51:54 +0200

    add recipe page

--- server.js
+++ server.js
@@ -85,11 +85,8 @@ function homeHandler(res,req){
 
 app.get('/sign' ,signHandler);
 function signHandler(req,res){
-
   res.render('pages/sign');
 }
-
-
 
 app.post('/signform', function(request, response) {
 	var username = request.body.username;
@@ -108,6 +105,199 @@ app.post('/signform', function(request, response) {
       })
 	
 }});
+
+//..................................................................................recipe part
+app.get('/searches',(req,res)=>{
+  res.render('./pages/recipe');
+})
+
+app.post('/recipeSearch' , handleRecipeSearch);
+
+function handleRecipeSearch(req, res) {
+  let recipeSearchWord = req.body.recipeSearch;
+
+  let RECIPE_APPKey = process.env.RECIPE_APP_KEY;
+
+  let RECIPE_APPID = process.env.RECIPE_APPID;
+  
+  let url = `https://api.edamam.com/search?q=${recipeSearchWord}&app_id=${RECIPE_APPID}&app_key=${RECIPE_APPKey}`;
+
+  console.log(url);
+
+  superagent.get(url).then (results => {
+
+ 
+      let recipeData = results.body.hits;
+      console.log(recipeData);
+       
+      let recipeArr = recipeData.map(value =>{
+
+          const recipeObject = new Recipe(value.recipe);
+          return recipeObject;
+      })
+      console.log(recipeArr);
+
+      res.render('./pages/recipeResult', {recipeArray:recipeArr});
+  })
+}
+
+
+//........................................
+app.post('/caloriesMeal' , handleCaloriesSearch);
+
+function handleCaloriesSearch(req, res) {
+  let caloriesSearch0 = req.body.caloriesSearch;
+
+  let apiKey = process.env.apiKey;
+
+  // let url = `https://api.spoonacular.com/mealplanner/generate?apiKey=${apiKey}&timeFrame=day&targetCalories=${caloriesSearch0}`;
+  let url = `https://api.spoonacular.com/recipes/complexSearch?maxCalories=${caloriesSearch0}&apiKey=${apiKey}`
+
+  console.log(url);
+
+  superagent.get(url).then (results => {
+      let caloriesDataMeals = results.body.results;
+      let caloriesMealsArr = caloriesDataMeals.map(value =>{
+          return value.id;
+      })
+      console.log(caloriesMealsArr);
+
+      let arr =[];
+      let count = 0;
+      let mealsArr = caloriesMealsArr.map(value =>{
+        let mealsUrl = `https://api.spoonacular.com/recipes/${value}/information?apiKey=${apiKey}&includeNutrition=false`;
+        superagent.get(mealsUrl).then(results => {
+
+          let mealsData = results.body;
+              const mealsObject = new Meal(mealsData);
+              arr.push(mealsObject);
+              console.log(count);
+              count++;
+              if(caloriesMealsArr.length === count){
+          res.render('./pages/caloriesPageResult', {mealsArray:arr});
+        }
+      })
+      })
+  })
+}
+
+//........................
+app.post('/caloriesForDay' , handleCaloriesForDay);
+
+function handleCaloriesForDay(req, res) {
+  let caloriesSearch0 = req.body.caloriesDaySearch;
+
+  let apiKey = process.env.apiKey;
+
+  let url = `https://api.spoonacular.com/mealplanner/generate?apiKey=${apiKey}&timeFrame=day&targetCalories=${caloriesSearch0}`;
+  // let url = `https://api.spoonacular.com/recipes/complexSearch?maxCalories=${caloriesSearch0}&apiKey=cb1c464d94f142c08b156c5beddade8b`
+
+  console.log(url);
+
+  superagent.get(url).then (results => {
+
+      let caloriesData0 = results.body.nutrients.calories;
+      let caloriesData1 = results.body.nutrients.protein;
+      let caloriesData2 = results.body.nutrients.fat;
+      let caloriesData3 = results.body.nutrients.carbohydrates;
+
+      console.log(caloriesData0,caloriesData1,caloriesData2,caloriesData3);
+
+      let caloriesDataMeals = results.body.meals;
+       
+      let caloriesMealsArr = caloriesDataMeals.map(value =>{
+          return value.id;
+      })
+      console.log(caloriesMealsArr);
+
+      let arr =[];
+      let count = 0;
+      let mealsArr = caloriesMealsArr.map(value =>{
+        let mealsUrl = `https://api.spoonacular.com/recipes/${value}/information?apiKey=${apiKey}&includeNutrition=false`;
+        superagent.get(mealsUrl).then(results => {
+
+          let mealsData = results.body;
+              const mealsObject = new Meal(mealsData);
+              arr.push(mealsObject);
+              console.log(count);
+              count++;
+              if(caloriesMealsArr.length === count){
+          res.render('./pages/caloriesDayResult', {mealsForDayArray:arr});
+        }
+      })
+      })
+  })
+}
+
+//..............................
+
+app.post('/searches/new' , handleSearch);
+
+function handleSearch(req, res) {
+  let searchWay = req.body.searchWay;
+
+  let apiKey = process.env.apiKey;
+
+  // let url = `https://api.spoonacular.com/mealplanner/generate?apiKey=${apiKey}&timeFrame=day&targetCalories=${caloriesSearch0}`;
+  let url = `https://api.spoonacular.com/recipes/complexSearch?type=${searchWay}&apiKey=${apiKey}`
+
+  console.log(url);
+
+  superagent.get(url).then (results => {
+
+
+      let caloriesDataMeals = results.body.results;
+      let caloriesMealsArr = caloriesDataMeals.map(value =>{
+          return value.id;
+      })
+      console.log(caloriesMealsArr);
+
+      let arr =[];
+      let count = 0;
+      let mealsArr = caloriesMealsArr.map(id =>{
+        let mealsUrl = `https://api.spoonacular.com/recipes/${id}/information?apiKey=${apiKey}&includeNutrition=false`;
+        superagent.get(mealsUrl).then(results => {
+
+          let mealsData = results.body;
+              const mealsObject = new Meal(mealsData);
+              arr.push(mealsObject);
+              console.log(count);
+              count++;
+              if(caloriesMealsArr.length === count){
+          res.render('./pages/searchPage', {searchArray:arr});
+        }
+      })
+    })
+
+  })
+}
+
+//....................................
+
+function Recipe(data){
+  this.label = data.label;
+  this.image = data.image;
+  this.ingredientLines = data.ingredientLines;
+  this.calories = data.calories.toFixed(2);
+  this.fat = data.totalNutrients.FAT.quantity.toFixed(2);
+  this.Carbs = data.totalNutrients.CHOCDF.quantity.toFixed(2);
+  this.Protein = data.totalNutrients.PROCNT.quantity.toFixed(2);
+  this.Cholesterol = data.totalNutrients.CHOLE.quantity.toFixed(2);
+  }
+
+  function Nutrients(data){
+    this.calories = data.calories;
+    this.protein = data.protein;
+    this.fat = data.fat;
+    this.carbohydrates = data.carbohydrates;
+  }
+
+  function Meal(data){
+    this.title = data.title;
+    this.image = data.image;
+    this.summary = data.summary;
+    this.instructions = data.instructions;
+  }
 
 client.connect()
     .then(() => {
